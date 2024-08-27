0e3bcc8d1e6a97e2c1adac6d23a1991a76284591
t-fi <tobias.fischer@posteo.net>
2024-08-27 12:01:31 +0200

    reading data works, need to figure out coding next

--- main/haxxboard.cpp
+++ main/haxxboard.cpp
@@ -6,6 +6,7 @@
 #include "key_maps.h"
 #include "led.h"
 #include "usb.h"
+#include <vector>
 #include <iostream>
 #include <driver/gpio.h>
 #include <sys/stat.h>
@@ -26,11 +27,8 @@ constexpr uint8_t write_mask = 0x80;
 constexpr uint8_t read_mask = 0xA0;
 
 // Register config values for this demo
-constexpr uint8_t sysconfig_1_data = 0x00;
 constexpr uint8_t sysconfig_1_addr = 0x03;
-constexpr uint8_t feedconfig_1_data = 0x81; //0x01 = relative, 0x03 = absolute, 0x80 = invert Y-axis
 constexpr uint8_t feedconfig_1_addr = 0x04;
-constexpr uint8_t feedconfig_2_data = 0x1C; // disable scroll, disable rtap
 constexpr uint8_t feedconfig_2_addr = 0x05;
 constexpr uint8_t z_idle_count = 0x05;
 constexpr uint8_t packetbyte_0_address = 0x12;
@@ -45,22 +43,111 @@ constexpr uint16_t pinnacle_y_upper = 1471; // max "reachable" Y value
 constexpr uint16_t pinnacle_x_range = (pinnacle_x_upper - pinnacle_x_lower);
 constexpr uint16_t pinnacle_y_range = (pinnacle_y_upper - pinnacle_y_lower);
 
+struct dummy {
+    uint8_t bit0: 1 = 0;
+    uint8_t bit1: 1 = 0;
+    uint8_t bit2: 1 = 0;
+    uint8_t bit3: 1 = 0;
+    uint8_t bit4: 1 = 0;
+    uint8_t bit5: 1 = 0;
+    uint8_t bit6: 1 = 0;
+    uint8_t bit7: 1 = 0;
+};
 
-uint8_t read_rap(i2c_master_dev_handle_t dev_handle, uint8_t address) {
+union Status1 {
+    struct {
+        bool bit0: 1 = false;
+        bool bit1: 1 = false;
+        bool software_data_ready: 1 = false;
+        bool commmand_complete: 1 = false;
+        bool bit4: 1 = false;
+        bool bit5: 1 = false;
+        bool bit6: 1 = false;
+        bool bit7: 1 = false;
+    } bits{};
+
+    uint8_t value;
+};
+
+static_assert(sizeof(Status1) == 1, "Size of Status1 should be 1 byte.");
+
+union SysConfig1 {
+    struct {
+        bool reset: 1 = false; // 1 = reset
+        bool shutdown: 1 = false; // 1 = shutdown
+        bool sleep_enable: 1 = false; // 1 = low power mode
+        bool bit3: 1 = false;
+        bool bit4: 1 = false;
+        bool bit5: 1 = false;
+        bool bit6: 1 = false;
+        bool bit7: 1 = false;
+    } bits{};
+
+    uint8_t value;
+};
+
+static_assert(sizeof(SysConfig1) == 1, "Size of SysConfig1 should be 1 byte.");
+
+union FeedConfig1 {
+    struct {
+        bool feed_enable: 1 = false; // 1 = feed, 0 = no feed
+        bool data_mode: 1 = false; // 1 = absolute, 0 = relative
+        bool filter_disable: 1 = false; // 1 = no filter, 0 = filter
+        bool x_disable: 1 = false; // 1 = no x data, 0 = x data
+        bool y_disable: 1 = false; // 1 = no y data, 0 = y data
+        bool bit5: 1 = false;
+        bool x_data_invert: 1 = false; // 1 = y max to 0, 0 = 0 to y max
+        bool y_data_invert: 1 = false; // 1 = x max to 0, 0 = 0 to x max
+    } bits{};
+
+    uint8_t value;
+};
+
+static_assert(sizeof(FeedConfig1) == 1, "Size of FeedConfig1 should be 1 byte.");
+
+union FeedConfig2 {
+    struct {
+        bool intellimouse_enable: 1 = false;
+        bool all_taps_disable: 1 = false;
+        bool secondary_tap_disable: 1 = false;
+        bool scroll_disable: 1 = false;
+        bool glide_extend_disable: 1 = false;
+        bool bit5: 1 = false;
+        bool bit6: 1 = false;
+        bool swap_x_y: 1 = false;
+    } bits{};
+
+    uint8_t value;
+};
+
+union position {
+    struct {
+        uint8_t base_value = 0;
+        uint8_t sign = 0;
+    } bits{};
+
+    int16_t value;
+};
+
+
+static_assert(sizeof(FeedConfig2) == 1, "Size of FeedConfig2 should be 1 byte.");
+
+
+std::vector<uint8_t> read_rap(i2c_master_dev_handle_t dev_handle, uint8_t address, uint8_t numBytes) {
     const uint8_t read_cmd = address | read_mask;
     esp_err_t ret = i2c_master_transmit(dev_handle, &read_cmd, 1, -1);
+    std::vector<uint8_t> data(numBytes);
 
     if (ret != ESP_OK) {
         ESP_LOGE("I2C", "Failed to send read command with address: 0x%.2x", address);
-        return 0;
+        return {};
     }
 
-    uint8_t data;
-    ret = i2c_master_receive(dev_handle, &data, 1, -1);
+    ret = i2c_master_receive(dev_handle, data.data(), numBytes, -1);
 
     if (ret != ESP_OK) {
         ESP_LOGE("I2C", "Failed to receive data from address: 0x%.2x", address);
-        return 0;
+        return {};
     }
 
     return data;
@@ -75,10 +162,11 @@ void write_rap(i2c_master_dev_handle_t dev_handle, uint8_t address, uint8_t data
 }
 
 
-char *to_binary(uint8_t num) {
-    static char bin[9];
-    bin[8] = '\0';
-    for (int i = 7; i >= 0; i--) {
+template<typename T>
+char *to_binary(T num) {
+    static char bin[sizeof(T) * 8 + 1] = {0};
+    int n = sizeof(T) * 8 - 1;
+    for (int i = n; i >= 0; --i) {
         bin[i] = (num & 1) ? '1' : '0';
         num >>= 1;
     }
@@ -87,6 +175,20 @@ char *to_binary(uint8_t num) {
 
 extern "C" void app_main(void) {
     ESP_LOGI("General", "Hi from the start of main");
+
+    /*position a{.bits = {.base_value = 3}};
+    position b{.bits = {.base_value = 255}};
+    position c{.bits = {.base_value = 255, .sign = 255}};
+    position d{.bits = {.base_value = 0}};
+    position e{.bits = {.base_value = 0, .sign = 255}};
+
+    ESP_LOGI("General", "value 3: %d", a.value);
+    ESP_LOGI("General", "value 255: %d", b.value);
+    ESP_LOGI("General", "value -1: %d", c.value);
+    ESP_LOGI("General", "value 0: %d", d.value);
+    ESP_LOGI("General", "value -255: %d", e.value);
+
+    return;*/
 
     // set up hardware data ready probe pin
     constexpr gpio_config_t gpio_cfg = {
@@ -148,7 +250,7 @@ extern "C" void app_main(void) {
     }
 
     // Read back the same register
-    const uint8_t read_back_data = read_rap(dev_handle, 0x02);
+    const uint8_t read_back_data = read_rap(dev_handle, 0x02, 1)[0];
 
     // Check if the value is set
     if (read_back_data != 0x00) {
@@ -156,16 +258,26 @@ extern "C" void app_main(void) {
         return;
     }
 
+    write_rap(dev_handle, sysconfig_1_addr, SysConfig1().value);
+    write_rap(dev_handle, feedconfig_2_addr, FeedConfig2().value);
+    write_rap(dev_handle, feedconfig_1_addr, FeedConfig1{
+                  .bits = {
+                      .feed_enable = true
+                  }
+              }.value);
 
-    ESP_LOGI("DATA", " === READING REGISTERS ===");
-    for (uint8_t reg = 0x00; reg < 0x18; reg++) {
-        ESP_LOGI("DATA", "Reading register %#.2x:", reg);
-        const uint8_t data = read_rap(dev_handle, reg);
-        ESP_LOGI("DATA", " result: %s", to_binary(data));
+    while (true) {
+        if (gpio_get_level(dr_pin)) {
+            ESP_LOGI("I2C", "got data");
+            const auto data = read_rap(dev_handle, 0x12, 3);
+            for (int i = 0; i < data.size(); i++) {
+                ESP_LOGI("I2C", "data[%d]: %s", i, to_binary(data[i]));
+            }
+            write_rap(dev_handle, 0x02, 0x00);
+        }
     }
 
-
-    ESP_LOGI("General", "returning");
+    ESP_LOGI("General", "End of main");
 }
 
 
