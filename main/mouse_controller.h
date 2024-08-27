7075d2314c723323176c75bcefe673d6996ccf65
t-fi <tobias.fischer@posteo.net>
2024-08-28 00:07:22 +0200

    note to future self

--- main/mouse_controller.h
+++ main/mouse_controller.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "cirque_pinnacle.h"
-#include <functional>
 
 constexpr gpio_num_t touchpad_sda_pin = GPIO_NUM_2;
 constexpr gpio_num_t touchpad_scl_pin = GPIO_NUM_1;
 constexpr gpio_num_t touchpad_data_ready_pin = GPIO_NUM_3;
 
+// alternative implementation:
+// mouse controller is a lambda that returns a lambda
+// and passes around the touch pad and history as arguments
 
 class MouseController {
 public:
