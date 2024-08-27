92e5ab9942575fbd61c98ba7f19e1e5c2f750763
t-fi <tobias.fischer@posteo.net>
2024-08-27 23:26:08 +0200

    works, but can be simplified... maybe make it all free functions and pass common data around? or make that data static?!

--- main/mouse_controller.cpp
+++ main/mouse_controller.cpp
@@ -1,17 +1,21 @@
 #include "mouse_controller.h"
 #include "esp_log.h"
 
+void MouseController::tick() {
+    (this->*_current_state_function)();
+}
+
 void MouseController::idle() {
     ESP_LOGI("StateMachine", "Hi from idle");
-    this->tick = &initial_touch;
+    this->_current_state_function = &MouseController::initial_touch;
 }
 
 void MouseController::initial_touch() {
     ESP_LOGI("StateMachine", "Hi from initial touch");
-    this->tick = &tracking;
+    this->_current_state_function = &MouseController::tracking;
 }
 
 void MouseController::tracking() {
     ESP_LOGI("StateMachine", "Hi from tracking");
-    this->tick = &idle;
+    this->_current_state_function = &MouseController::idle;
 }
--- main/mouse_controller.h
+++ main/mouse_controller.h
@@ -9,6 +9,11 @@ constexpr gpio_num_t touchpad_data_ready_pin = GPIO_NUM_3;
 
 
 class MouseController {
+public:
+    MouseController() = default;
+
+    void tick();
+
 private:
     TouchPad _touch_pad{touchpad_sda_pin, touchpad_scl_pin, touchpad_data_ready_pin};
 
@@ -18,9 +23,5 @@ private:
 
     void tracking();
 
-public:
-    MouseController() = default;
-
-    using StateFunction = decltype(&MouseController::idle);
-    StateFunction tick{&MouseController::idle};
+    decltype(&MouseController::idle) _current_state_function{&MouseController::idle};
 };
