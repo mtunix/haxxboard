169aace656a92d35c49ad1448ed9353311174cd6
t-fi <tobias.fischer@posteo.net>
2024-09-01 16:08:22 +0200

    unique pointer version

--- main/mouse_controller.cpp
+++ main/mouse_controller.cpp
@@ -1,24 +1,27 @@
 #include "mouse_controller.h"
 #include "esp_log.h"
 
+
+std::unique_ptr<State> Idle::update(Movement movement) {
+    ESP_LOGI("StateMachine", "Hi from idle");
+    return std::make_unique<InitialTouch>();
+}
+
+std::unique_ptr<State> InitialTouch::update(Movement movement) {
+    ESP_LOGI("StateMachine", "Hi from initial touch");
+    return std::make_unique<Tracking>();
+}
+
+std::unique_ptr<State> Tracking::update(Movement movement) {
+    ESP_LOGI("StateMachine", "Hi from tracking");
+    return std::make_unique<Idle>();
+}
+
 void MouseController::tick() {
-    (this->*_current_state_function)();
     if (const auto data = _touch_pad.get_data()) {
         ESP_LOGI("StateMachine", "got data: %d", data->z);
+        _latest_movement.from = _latest_movement.to;
+        _latest_movement.to = data.value();
+        _current_state = _current_state->update(_latest_movement);
     }
-}
-
-void MouseController::idle() {
-    ESP_LOGI("StateMachine", "Hi from idle");
-    this->_current_state_function = &MouseController::initial_touch;
-}
-
-void MouseController::initial_touch() {
-    ESP_LOGI("StateMachine", "Hi from initial touch");
-    this->_current_state_function = &MouseController::tracking;
-}
-
-void MouseController::tracking() {
-    ESP_LOGI("StateMachine", "Hi from tracking");
-    this->_current_state_function = &MouseController::idle;
 }
--- main/mouse_controller.h
+++ main/mouse_controller.h
@@ -1,29 +1,43 @@
 #pragma once
 
 #include "cirque_pinnacle.h"
+#include <functional>
+#include <memory>
 
 constexpr gpio_num_t touchpad_sda_pin = GPIO_NUM_2;
 constexpr gpio_num_t touchpad_scl_pin = GPIO_NUM_1;
 constexpr gpio_num_t touchpad_data_ready_pin = GPIO_NUM_3;
 
-// alternative implementation:
-// mouse controller is a lambda that returns a lambda
-// and passes around the touch pad and history as arguments
+struct Movement {
+    TouchData from, to;
+};
+
+struct State {
+    virtual ~State() = default;
+
+    virtual std::unique_ptr<State> update(Movement movement) = 0;
+};
+
+struct Idle final : State {
+    std::unique_ptr<State> update(Movement movement) override;
+};
+
+struct InitialTouch final : State {
+    std::unique_ptr<State> update(Movement movement) override;
+};
+
+struct Tracking final : State {
+    std::unique_ptr<State> update(Movement movement) override;
+};
 
 class MouseController {
 public:
     MouseController() = default;
 
-    void tick();
+    auto tick() -> void;
 
 private:
     TouchPad _touch_pad{touchpad_sda_pin, touchpad_scl_pin, touchpad_data_ready_pin};
-
-    void idle();
-
-    void initial_touch();
-
-    void tracking();
-
-    void (MouseController::*_current_state_function)(){&MouseController::idle};
+    std::unique_ptr<State> _current_state{std::make_unique<Idle>()};
+    Movement _latest_movement{};
 };
