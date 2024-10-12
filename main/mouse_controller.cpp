a78ee87b796b557ea99b4a8fda44592004c2af83
t-fi <tobias.fischer@posteo.net>
2024-10-13 01:13:21 +0200

    kinda fast mouse

--- main/mouse_controller.cpp
+++ main/mouse_controller.cpp
@@ -37,20 +37,20 @@ namespace {
             return MovementKind::new_position;
         }
 
-        ESP_LOGI("StateMachine", "No state matches, this shouldn't happen...");
+        //ESP_LOGI("StateMachine", "No state matches, this shouldn't happen...");
         return MovementKind::touch_down; // need some return value
     }
 }
 
 
 std::unique_ptr<State> Idle::transition(const TouchData &current_touch) {
-    ESP_LOGI("State Machine", "Idle transition");
+    //ESP_LOGI("State Machine", "Idle transition");
     switch (movement_to_kind(_last_touch, current_touch)) {
         case MovementKind::stay_off:
-            ESP_LOGI("State Machine", "Idle stay off");
+            //ESP_LOGI("State Machine", "Idle stay off");
             return std::make_unique<Idle>(current_touch);
         case MovementKind::touch_down:
-            ESP_LOGI("State Machine", "Idle touch down");
+            //ESP_LOGI("State Machine", "Idle touch down");
             return std::make_unique<Tracking>(current_touch);
         case MovementKind::lift_off:
             ESP_LOGE("State Machine", "Idle state machine got liftoff, impossible!");
@@ -66,7 +66,7 @@ std::unique_ptr<State> Idle::transition(const TouchData &current_touch) {
 }
 
 std::unique_ptr<State> Tracking::transition(const TouchData &current_touch) {
-    ESP_LOGI("State Machine", "Tracking transition");
+    //ESP_LOGI("State Machine", "Tracking transition");
     switch (movement_to_kind(_last_touch, current_touch)) {
         case MovementKind::stay_off:
             ESP_LOGE("State Machine", "Tracking state machine got stay_off, impossible!");
@@ -75,33 +75,35 @@ std::unique_ptr<State> Tracking::transition(const TouchData &current_touch) {
             ESP_LOGE("State Machine", "Tracking state machine got touch_down, impossible!");
             break;
         case MovementKind::lift_off:
-            ESP_LOGI("State Machine", "Tracking liftoff");
+            //ESP_LOGI("State Machine", "Tracking liftoff");
             return std::make_unique<Idle>(current_touch);
         case MovementKind::same_position:
-            ESP_LOGI("State Machine", "Tracking same position");
+            //ESP_LOGI("State Machine", "Tracking same position");
             return std::make_unique<Tracking>(current_touch);
         case MovementKind::new_position:
-            ESP_LOGI("State Machine", "Tracking new position");
-            const int dx = current_touch.x - _last_touch.x;
+            //ESP_LOGI("State Machine", "Tracking new position");
+            int dx = current_touch.x - _last_touch.x;
+        //dx /= 4;
             const int8_t dx_clamped = static_cast<int8_t>(std::clamp(
                 dx, static_cast<int>(std::numeric_limits<int8_t>::min()),
                 static_cast<int>(std::numeric_limits<int8_t>::max())));
-            const int dy = current_touch.y - _last_touch.y;
+            int dy = current_touch.y - _last_touch.y;
+        //dy /= 4;
             const int8_t dy_clamped = static_cast<int8_t>(std::clamp(
                 dy, static_cast<int>(std::numeric_limits<int8_t>::min()),
                 static_cast<int>(std::numeric_limits<int8_t>::max())));
             ESP_LOGI("State Machine", "sending mouse report: %d %d", dx, dy);
             tud_hid_mouse_report(HID_ITF_PROTOCOL_MOUSE, 0x00, dx_clamped, dy_clamped, 0, 0);
-            ESP_LOGI("State Machine", "mouse report sent");
+        //ESP_LOGI("State Machine", "mouse report sent");
             return std::make_unique<Tracking>(current_touch);
     }
-    ESP_LOGI("State Machine", "Tracking impossible state");
+    //ESP_LOGI("State Machine", "Tracking impossible state");
     return nullptr; // this shouldn't happen
 }
 
 void MouseController::tick() {
     if (const auto data = _touch_pad.get_data()) {
-        ESP_LOGI("State Machine", "got data: x y z %d %d %d", data->x, data->y, data->z);
+        //ESP_LOGI("State Machine", "got data: x y z %d %d %d", data->x, data->y, data->z);
         _state = _state->transition(data.value());
     }
 }
