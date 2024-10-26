8da92ed29b9931016fc4023b7d9f9ca19c29e5a4
t-fi <tobias.fischer@posteo.net>
2024-10-26 14:19:27 +0200

    broken key scan, missing the righ headers. Committing so i can check out an older version

--- main/haxxboard.cpp
+++ main/haxxboard.cpp
@@ -1,15 +1,28 @@
+#include <projdefs.h>
+
 #include "esp_log.h"
 #include "usb.h"
+#include "key_scan.h"
+#include "task.h"
 
 #include "mouse_controller.h"
 
 extern "C" void app_main(void) {
     init_usb();
+
+    init_gpios();
+
     ESP_LOGI("General", "Hi from the start of main");
     MouseController mouse_controller;
+    int i = 0;
     while (true) {
         if (usb_mounted())
             mouse_controller.tick();
+        send_pressed_keys();
+        vTaskDelay(pdMS_TO_TICKS(10));
+        if ((++i % 100) == 0) {
+            ESP_LOGI("General", "HI");
+        }
     }
     ESP_LOGI("General", "End of main");
 }
@@ -18,7 +31,7 @@ extern "C" void app_main(void) {
 /*
 {
     // set_color(16, 4, 2);
-    init_gpios();
+
     init_usb();
     // set_color(0, 16, 0);
 
